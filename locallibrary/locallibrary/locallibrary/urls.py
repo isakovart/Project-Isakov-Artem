1462d4e6a6e7101fd77d9edb1b28d021550a6658
isakovart <102592246+isakovart@users.noreply.github.com>
2022-03-29 14:21:22 +0300

    Delete locallibrary/locallibrary directory

--- locallibrary/locallibrary/locallibrary/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls'), name="catalog"),
-    path('accounts/', include('django.contrib.auth.urls')),
-]
